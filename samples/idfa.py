627c34c8e542f6b73180408c68ab79c156b5ae12
Guilherme M. Trein <guitrein@yahoo.com.br>
2014-07-25 07:38:33 -0400

    Improve formatting

--- samples/idfa.py
+++ samples/idfa.py
@@ -5,8 +5,13 @@ from decrypter import DecrypterIdfa
 
 encryption_encoded_key = 'AAECAwQFBgcICQoLDA0ODxAREhMUFRYXGBkaGxwdHh8,'
 integrity_encoded_key = 'Hx4dHBsaGRgXFhUUExIREA8ODQwLCgkIBwYFBAMCAQA,'
-encrypted_hashed_idfa = binascii.unhexlify('51928A6600000000AAAAAACEAAAAAACEB30EDBA1D938ACFB12C91670AB8D01F31E434557')
-encrypted_advertising_id = binascii.unhexlify('51928A6600000000AAAAAACEAAAAAACED2C32DF16B1658B8447591BE4BA853922C55ABAD')
+
+encrypted_hashed_idfa = binascii.unhexlify(
+    '51928A6600000000AAAAAACEAAAAAACEB30E'
+    'DBA1D938ACFB12C91670AB8D01F31E434557')
+encrypted_advertising_id = binascii.unhexlify(
+    '51928A6600000000AAAAAACEAAAAAACED2C3'
+    '2DF16B1658B8447591BE4BA853922C55ABAD')
 
 long_ciphertext = {
     'encrypted_hashed_idfa': encrypted_hashed_idfa,
@@ -17,13 +22,13 @@ decrypter = DecrypterIdfa(encryption_encoded_key, integrity_encoded_key)
 idfa_set = decrypter.decryption(long_ciphertext['encrypted_hashed_idfa'])
 
 print {
-    'idfa_hex': binascii.hexlify(idfa_set['idfa']),
+    'idfa_hex': idfa_set['idfa_hex'],
     'datetime': idfa_set['datetime'],
 }
 
 idfa_set = decrypter.decryption(long_ciphertext['encrypted_advertising_id'])
 
 print {
-    'android_id_hex': binascii.hexlify(idfa_set['idfa']),
+    'android_id_hex': idfa_set['idfa_hex'],
     'datetime': idfa_set['datetime'],
-};
+}
--- samples/price.py
+++ samples/price.py
@@ -6,7 +6,8 @@ from decrypter import DecrypterPrice
 encryption_encoded_key = 'sIxwz7yw62yrfoLGt12lIHKuYrK_S5kLuApI2BQe7Ac'
 integrity_encoded_key = 'v3fsVcMBMMHYzRhi7SpM0sdqwzvAxM6KPTu9OtVod5I'
 long_ciphertext = 'SjpvRwAB4kB7jEpgW5IA8p73ew9ic6VZpFsPnA'
-safe_long_ciphertext = base64.urlsafe_b64decode(long_ciphertext + '=' * (4 - len(long_ciphertext) % 4))
+safe_long_ciphertext = base64.urlsafe_b64decode(
+    long_ciphertext + '=' * (4 - len(long_ciphertext) % 4))
 
 decrypt_price = DecrypterPrice(encryption_encoded_key, integrity_encoded_key)
 print decrypt_price.decryption(safe_long_ciphertext)
